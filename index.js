339ca87c4a47d8f88b4109c34d21a4c64fc7f9b7
horiagrama <hooria85@yahoo.com>
2015-09-09 16:01:13 +0300

    Update index.js
    updated listen

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 a
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('port', process.env.PORT ||process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
 
 app.get('/', function(req, res){
@@ -35,6 +35,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(app.get('port'), function(){
   console.log('listening at %s:%d:', app.get('ip'),app.get('port'));
 });
