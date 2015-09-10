5167d8592c7af434c71515bde5a1e4457bf72920
Horia Grama <horia.grama@breezemobile.ro>
2015-09-10 17:59:47 +0300

    revenire 'chat message'?!

--- index.js
+++ index.js
@@ -2,70 +2,68 @@
 // var http = require('http').Server(app);
 // var io = require('socket.io')(http);
 // 
-// app.set('port', process.env.PORT ||process.env.OPENSHIFT_NODEJS_PORT || 3000);
-// // app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0"); 
+// app.set('port', process.env.PORT || 3000);
+// 
+// var port = app.get('port');
 // 
 // app.get('/', function(req, res){
-//   res.sendfile(__dirname + '/index.html');
+//   res.sendFile(__dirname + '/index.html');
 // });
 // 
-// // io.on('connection', function(socket){
-// //   console.log('a user connected');
-// //   socket.on('disconnect', function(){
-// //     console.log('user disconnected');
-// // 	});
-// // });
-// 
-// 
-// // io.on('connection', function(socket){
-// //   socket.on('chat message', function(msg){
-// //     console.log('message: ' + msg);
-// //   });
-// // });
-// 
 // io.on('connection', function(socket){
+// 
 // 	console.log('Connection to client established');
-// 	
 // 	socket.on('disconnect', function(){
 //     	console.log('user disconnected');
 // 	});
-// 
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
+// 	
+// 	io.emit('#conn', 'User connected');
+// 	
+//   socket.on('#msg', function(msg){
+//     io.emit('#msg', msg);
 //   });
 // });
 // 
 // http.listen(app.get('port'), function(){
-//   console.log('listening at %s:%d:', app.get('ip'),app.get('port'));
+//   console.log('listening on *:' + port);
 // });
-
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.set('port', process.env.PORT || 3000);
-
-var port = app.get('port');
+app.set('ip', process.env.IP || "127.0.0.1"); 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+// 	});
+// });
 
+
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+// });
+
+io.on('connection', function(socket){
 	console.log('Connection to client established');
 	socket.on('disconnect', function(){
     	console.log('user disconnected');
 	});
-	
-	io.emit('#conn', 'User connected');
-	
-  socket.on('#msg', function(msg){
-    io.emit('#msg', msg);
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
 http.listen(app.get('port'), function(){
-  console.log('listening on *:' + port);
+  console.log('listening at %s:%d:', app.get('ip'),app.get('port'));
 });
