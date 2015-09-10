81e6d60765bc89f6a4d9af1031b6805da1d45769
Grama Horia <hooria85@yahoo.com>
2015-09-10 20:44:00 +0300

    revert dimineata

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// 
-// app.set('port', process.env.PORT || 3000);
-// 
-// var port = app.get('port');
-// 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-// 
-// io.on('connection', function(socket){
-// 
-// 	console.log('Connection to client established');
-// 	socket.on('disconnect', function(){
-//     	console.log('user disconnected');
-// 	});
-// 	
-// 	io.emit('#conn', 'User connected');
-// 	
-//   socket.on('#msg', function(msg){
-//     io.emit('#msg', msg);
-//   });
-// });
-// 
-// http.listen(app.get('port'), function(){
-//   console.log('listening on *:' + port);
-// });
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('port', process.env.PORT || 3000);
-app.set('ip', process.env.IP || "127.0.0.1"); 
+app.set('port', process.env.PORT ||process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
