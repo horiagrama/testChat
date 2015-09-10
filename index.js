7c25c4df972d1c49f70d602fcc2ffb279e1288ed
Horia Grama <horia.grama@breezemobile.ro>
2015-09-10 13:50:36 +0300

    test new listen

--- index.js
+++ index.js
@@ -44,11 +44,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('port', process.env.PORT ||process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('port', process.env.PORT || 3000);
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+var server = http.createServer(app);
+server.listen(app.get('port'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
@@ -56,6 +60,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(app.get('port'), function(){
-  console.log('listening on *:'+app.get('port'));
-});
+// http.listen(app.get('port'), function(){
+//   console.log('listening on *:'+app.get('port'));
+// });
