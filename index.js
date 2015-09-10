fbb03d08bc72b37388b5355841b3dc50b95c02bb
Horia Grama <horia.grama@breezemobile.ro>
2015-09-10 14:13:57 +0300

    /msg replaced chat message

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('/msg', function(msg){
+    io.emit('/msg', msg);
   });
 });
 
