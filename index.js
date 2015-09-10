6de4f55b615132d744ae2e6254f0e42322cd22d9
Horia Grama <horia.grama@breezemobile.ro>
2015-09-10 13:33:30 +0300

    node 0.10.x -> 0.11.16 custom port

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(app.get('port'), function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:'+app.get('port'));
 });
