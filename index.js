f376aad3416e9c3bc9640e6c14bfbd4d28a52bb8
Horia Grama <horia.grama@breezemobile.ro>
2015-09-10 12:15:27 +0300

    0.0.0.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.set('port', process.env.PORT ||process.env.OPENSHIFT_NODEJS_PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"); 
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0"); 
 
 http.listen(app.get('port'), function(){
   console.log('listening at %s:%d:', app.get('ip'),app.get('port'));
