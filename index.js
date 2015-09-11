2e404f6fdac6ffba9068332800e1728c122e6614
Horia Grama <horia.grama@breezemobile.ro>
2015-09-11 12:33:16 +0300

    back 2

--- index.js
+++ index.js
@@ -42,11 +42,6 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
   
-  socket.on('/join1'),function(msg){
-//   	socket.join(msg);
-//   	io.emit('chat message', msg);
-  }
-  
 });
 
 http.listen(app.get('port'), function(){
